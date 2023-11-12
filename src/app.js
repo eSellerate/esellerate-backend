2408643f94db5d24cedeadd22d4453f49979a6d5
Feivian4 <feivian21@gmail.com>
2023-11-11 22:11:04 -0600

    Cors origin localhost

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 // cors middleware
 app.use(cors({
-  origin: ['http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   exposedHeaders: ['Cookie']
 }))
