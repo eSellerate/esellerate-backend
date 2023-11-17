21154261f541b4441c8b09a2b0b306703209492f
Chris <a20110469@live.ceti.mx>
2023-11-17 01:00:52 -0600

    db options arreglado

--- src/models/MercadoLibreAuth.js
+++ src/models/MercadoLibreAuth.js
@@ -1,6 +1,6 @@
 import { DataTypes } from 'sequelize'
 import { sequelize } from '../database/database.js'
-import DbOptions from './dbOption.js'
+import DbOptions from './DbOptions.js'
 
 import MercadoLibreApp from './MercadoLibreApp.js'
 
