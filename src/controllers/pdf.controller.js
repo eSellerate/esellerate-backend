94e1abd74266e0f19ac620f33d84fb12e2ccf164
Christian Ochoa <a20110469@live.ceti.mx>
2024-01-04 20:57:25 -0600

    Cambios de formato de dinero en recibo

--- src/controllers/pdf.controller.js
+++ src/controllers/pdf.controller.js
@@ -2,15 +2,26 @@ import puppeteer from 'puppeteer'
 import path from 'node:path'
 import fs from 'node:fs'
 import Handlebars from 'handlebars'
+import currency from 'currency.js'
+
+function formatCurrency (value) {
+  const val = currency(value, {
+    presicion: 2,
+    separator: ','
+  }).format()
+  return val
+}
 
 export async function generateSaleOrder (req, res) {
   // Adapt the data for the template
   const data = req.body
   const items = data.map(item => ({ ...item.order_items[0], created: new Date(item.date_created).toLocaleDateString() }))
   items.forEach(item => { item.total = item.unit_price * item.quantity })
-  const totalEarnings = items.reduce((acc, val) => { return acc + val.total }, 0)
-  const totalFees = items.reduce((acc, val) => (acc + val.sale_fee), 0)
-  const total = totalEarnings - totalFees
+  let totalEarnings = items.reduce((acc, val) => { return acc + val.total }, 0)
+  let totalFees = items.reduce((acc, val) => (acc + val.sale_fee), 0)
+  const total = formatCurrency(totalEarnings - totalFees)
+  totalEarnings = formatCurrency(totalEarnings)
+  totalFees = formatCurrency(totalFees)
 
   // prepare and configure hbs template
   const file = path.join(global.__dirname, 'public', 'templates', 'report.hbs')
@@ -27,11 +38,11 @@ export async function generateSaleOrder (req, res) {
     total
   }
   const html = template(hbData)
-  const browser = await puppeteer.launch({
-    executablePath: '/snap/bin/chromium',
-    args: ['--no-sandbox']
-  })
-  // const browser =await puppeteer.launch()
+  // const browser = await puppeteer.launch({
+  //   executablePath: '/snap/bin/chromium',
+  //   args: ['--no-sandbox']
+  // })
+  const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.setContent(html)
   await page.pdf({
